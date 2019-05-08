7d3e4e33e01f993fae261743b2031609815bad86
Zhaopeng Gong <zhaopeng@ZhaopengdeiMac.local>
2019-05-08 10:43:46 +0800

    Originate the project.

---
+++ src/main/java/com/benben/wechat/mini/ThumbelinaApplication.java
@@ -0,0 +1,13 @@
+package com.benben.wechat.mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThumbelinaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThumbelinaApplication.class, args);
+    }
+
+}
